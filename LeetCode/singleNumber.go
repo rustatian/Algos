fffb1865bbdcfcbe52fc49d987da3d99258d74c6
Valery Piashchynski <piashchynski_valery@hotmail.com>
2018-01-22 22:37:59 +0300

    Add singleNumber task (https://leetcode.com/problems/single-number/description/)

---
+++ LeetCode/singleNumber.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println(singleNumber([]int{17, 12, 5, -6, 12, 4, 17, -5, 2, -3, 2, 4, 5, 16, -3, -4, 15, 15, -4, -5, -6}))
+}
+
+func singleNumber(nums []int) int {
+	sort.Ints(nums)
+	if len(nums) < 2 {
+		return nums[0]
+	}
+
+	for i := 0; i < len(nums)-1; i += 2 {
+		if nums[i] != nums[i+1] {
+			return nums[i]
+		}
+	}
+
+	return nums[len(nums)-1]
+}
