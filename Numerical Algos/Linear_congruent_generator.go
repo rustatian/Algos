d1abb3ecb47421e6e73114737e3c0d79451da47e
Valery Piashchynski <piashchynski_valery@hotmail.com>
2018-03-22 15:53:54 +0300

    Little update

--- Numerical Algos/Linear_congruent_generator.go
+++ Numerical Algos/Linear_congruent_generator.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
 )
 
-//X(n+1) = (A x X(n) + B) % M, where A, B, M are constant
-//X(0) - is initial number
-//https://ru.wikipedia.org/wiki/Линейный_конгруэнтный_метод
+// X(n+1) = (A x X(n) + B) % M, where A, B, M are constant
+// X(0) - is initial number
+// https://ru.wikipedia.org/wiki/Линейный_конгруэнтный_метод
 func main() {
 	var (
 		A = flag.Int("A", 2121, "A value")
@@ -15,11 +15,11 @@ func main() {
 		M = flag.Int("M", 44444444, "M value")
 	)
 
-	var Xn int = 0 //if we hold A and B, Xn will be seed
+	var Xn int = 0 // if we hold A and B, Xn will be seed
 	var Xn1 int
 
-	for i := 0; i < *M; i ++ {
-		Xn1 = ((*A) * (Xn) + (*B)) % (*M)
+	for i := 0; i < *M; i++ {
+		Xn1 = ((*A)*(Xn) + (*B)) % (*M)
 		Xn = Xn1
 	}
 
