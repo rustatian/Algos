f38c1ec4b347794febcfb7544a0ce80e89c7e493
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-06-03 09:35:00 +0300

    update comments on trolls task

--- CodeWars/C/7kyu/disemvowel_trolls/main.c
+++ CodeWars/C/7kyu/disemvowel_trolls/main.c
@@ -22,8 +22,10 @@ bool is_vowel(char c) {
 //solution must allocate all required memory
 //and return a free-able buffer to the caller.
 char *disemvowel(const char *str) {
+    // strlen don't encounter a 0-character
     const size_t len = strlen(str);
 
+    // to have a properly size we have to add 1 (include 0 character) to res
     char *const res = malloc(len + 1);
     size_t i_dst = 0;
 
@@ -39,6 +41,5 @@ char *disemvowel(const char *str) {
 int main() {
     // "Ths wbst s fr lsrs LL!"
     printf("result is: %s", disemvowel("This website is for losers LOL!"));
-//
 }
 
